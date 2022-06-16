a5a216c3c0a86f1658b68581e6eca23cfdd1f56e
piotr <upsol@protonmail.com>
2022-06-16 20:41:58 +0200

    some security

--- next.config.js
+++ next.config.js
@@ -1,6 +1,31 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+};
+const secureHeaders = [
+  {
+    key: "X-DNS-Prefetch-Control",
+    value: "on",
+  },
+  {
+    key: "X-XSS-Protection",
+    value: "1; mode=block",
+  },
 
-module.exports = nextConfig
+  {
+    key: "X-Frame-Options",
+    value: "SAMEORIGIN",
+  },
+
+  {
+    key: "X-Content-Type-Options",
+    value: "nosniff",
+  },
+];
+
+module.exports = {
+  nextConfig,
+  async headers() {
+    return [{ source: "/:path", headers: secureHeaders }];
+  },
+};
