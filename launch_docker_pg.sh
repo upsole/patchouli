5475fd021b4ce60e251b8679384b8c8504b00431
piotr <upsol@protonmail.com>
2022-07-15 23:01:45 +0200

    favicon

--- launch_docker_pg.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-POSTGRES_READY=1
-docker-compose up -d && while [ $POSTGRES_READY -eq 1 ]
-do 
-  echo "Checking postgres"
-  docker-compose exec db pg_isready && POSTGRES_READY=0 && break
-  sleep 2
-done
-yarn prisma migrate deploy && yarn prisma generate
